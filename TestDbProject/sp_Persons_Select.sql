f4d759e0a33ec9009c8e1d172156d8ef85b39291
GeneratorMotor <t15generator@yandex.ru>
2021-12-17 08:34:33 +0400

    #XDP-3 Создание ХП поиска Persons
    Создание хранимой процедуры для поиска
    сотрудника по ID

---
+++ TestDbProject/sp_Persons_Select.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[sp_Persons_Select]
+	@Id int -- идентификатор лица
+AS
+	SELECT 
+	Persons.Id,
+	Persons.Name,
+	Persons.Surname,
+	Persons.Patronumic,
+	Persons.DateBerf
+	from Persons
+	where Persons.Id = @Id
+END
